aeeecd98a386f73bb232917ee349be8a96627965
Do Hyun Jung(cirel31) <cirel31@gmail.com>
2024-01-07 20:35:29 +0900

    [Silver IV] Title: 덱 2, Time: 1460 ms, Memory: 70596 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -1,10 +1,10 @@
 import sys
-from collections import deque
+from _collections import deque
+
 
 input = sys.stdin.readline
 n = int(input())
 res = deque()
-
 for _ in range(n):
     x = list(map(int, input().split()))
     if x[0] == 1:
@@ -12,18 +12,31 @@ for _ in range(n):
     elif x[0] == 2:
         res.append(x[1])
     elif x[0] == 3:
-        print(res[0] if res else -1)
-        if res:
+        if len(res) > 0:
+            print(res[0])
             res.popleft()
+        else:
+            print(-1)
     elif x[0] == 4:
-        print(res[-1] if res else -1)
-        if res:
+        if len(res) > 0:
+            print(res[-1])
             res.pop()
+        else:
+            print(-1)
     elif x[0] == 5:
         print(len(res))
     elif x[0] == 6:
-        print(1 if not res else 0)
+        if len(res) > 0:
+            print(0)
+        else:
+            print(1)
     elif x[0] == 7:
-        print(res[0] if res else -1)
+        if len(res) > 0:
+            print(res[0])
+        else:
+            print(-1)
     elif x[0] == 8:
-        print(res[-1] if res else -1)
+        if len(res) > 0:
+            print(res[-1])
+        else:
+            print(-1)
