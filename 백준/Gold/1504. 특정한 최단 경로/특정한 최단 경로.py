8118bd539318b290a0bb878c57ea18f0650e2bc4
Do Hyun Jung(cirel31) <cirel31@gmail.com>
2024-01-08 11:02:11 +0900

    Update 특정한 최단 경로.py

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,43 +1,41 @@
-import heapq
 import sys
+from heapq import heappush, heappop
 
 
-def dijkstra(start):
-    queue = []
-    distance = [inf] * (n + 1)
+def solve(start):
+    que = []
+    distance = [inf for _ in range(N+1)]
     distance[start] = 0
-    heapq.heappush(queue, (0, start))
-    while queue:
-        length, idx = heapq.heappop(queue)
+    heappush(que, (start, 0))
+    while que:
+        idx, length = heappop(que)
         if length > distance[idx]:
             continue
-        for x, y in graph[idx]:
-            d = length + y
-            if d < distance[x]:
-                distance[x] = d
-                heapq.heappush(queue, (d, x))
+        for now, weight in graph[idx]:
+            dt = length + weight
+            if dt < distance[now]:
+                distance[now] = dt
+                heappush(que, (now, dt))
     return distance
 
 
-n, e = map(int, sys.stdin.readline().split())
-graph = [[] for i in range(n+1)]
-inf = sys.maxsize
-for _ in range(e):
-    a, b, c = map(int, sys.stdin.readline().split())
+input = sys.stdin.readline
+N, E = map(int, input().split())
+inf = 1e9
+graph = [[] for _ in range(N+1)]
+for _ in range(E):
+    a, b, c = map(int, input().split())
     graph[a].append((b, c))
     graph[b].append((a, c))
-v1, v2 = map(int, sys.stdin.readline().split())
+v1, v2 = map(int, input().split())
 
-res1 = dijkstra(1)
-res2 = dijkstra(v1)
-res3 = dijkstra(v2)
+# 다익스트라 알고리즘을 통해 시작 위치에 따른 최단 경로 결과 계산
+basic = solve(1)
+start_v1 = solve(v1)
+start_v2 = solve(v2)
 
-rst1 = res1[v1] + res2[v2] + res3[n]
-rst2 = res1[v2] + res2[n] + res3[v1]
-
-ans = min(rst1, rst2)
-if ans < inf:
-    print(ans)
-else:
-    print(-1)
-
+# 두 가지 경로 계산: 1 -> v1 -> v2 -> N, 1 -> v2 -> v1 -> N 후 최단 결과 반영
+rst_1 = basic[v1] + start_v1[v2] + start_v2[N]
+rst_2 = basic[v2] + start_v1[N] + start_v2[v1]
+ans = min(rst_1, rst_2)
+print(ans if ans < inf else -1)
