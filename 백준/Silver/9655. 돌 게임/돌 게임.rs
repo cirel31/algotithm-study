003bed095ba5a7fb1c8b566f9bf563ef4913b69e
Do Hyun Jung(cirel31) <cirel31@gmail.com>
2024-04-20 19:45:37 +0900

    [Silver V] Title: 돌 게임, Time: 4 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.rs
+++ 백준/Silver/9655. 돌 게임/돌 게임.rs
@@ -1,32 +1,13 @@
 use std::io;
 
-fn stone_game(n: usize) -> &'static str {
-    let mut dp = vec![false; n + 1];
-
-    if n >= 1 {
-        dp[1] = true;
-    }
-    if n >= 2 {
-        dp[2] = false;
-    }
-    if n >= 3 {
-        dp[3] = true;
-    }
-
-    for i in 4..=n {
-        dp[i] = !dp[i - 1] || (i >= 3 && !dp[i - 3]);
-    }
-
-    if dp[n] {
-        "SK"
-    } else {
-        "CY"
-    }
-}
-
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let n: usize = input.trim().parse().unwrap(); 
-    println!("{}", stone_game(n));
+    let n: usize = input.trim().parse().unwrap();
+
+    if n % 2 == 0 {
+        println!("CY");
+    } else {
+        println!("SK");
+    }
 }
